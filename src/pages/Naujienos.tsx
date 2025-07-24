f385e2e5dcfaf909723ffb44181a58af34c69db7
BboyHappy <linelistamos@gmail.com>
2025-07-24 19:02:46 +0300

    Update Naujienos.tsx

--- src/pages/Naujienos.tsx
+++ src/pages/Naujienos.tsx
@@ -12,14 +12,7 @@ const Naujienos = () => {
       date: "2025-07-22",
       image: "/lovable-uploads/66ff45ea-6901-482a-ae10-53331ae1901c.png",
       excerpt: "",
-      content: "Laukiame mažų ir didelių – mišri grupė nuo 3 metų iki kiek tik norėsite! 😊 Veiklų netrūks visiems: mažieji jojimo džiaugsmą patirs ant asilės Simos, o vyresni galės pajodinėti žirgais.
-
-Jeigu orai palankūs – maudynės ir vandens mūšiai garantuoti! Taip pat susipažinsime su avytėmis, ožiukais, maitinsime žąsiukus ir tyrinėsime gamtą Nemuno kilpų regioniniame parke.
-
-🕘 Atvykimas – 9:00
-🕔 Išvykimas – 17:00
-🍽 Kaina vaikui – 35 € (įskaičiuoti pietūs ir užkandžiai)
-👫 Grupėje – 10–14 vaikų, tad dėmesio užteks kiekvienam!
+      content: "Laukiame mažų ir didelių – mišri grupė nuo 3 metų iki kiek tik norėsite! 😊 Veiklų netrūks visiems: mažieji jojimo džiaugsmą patirs ant asilės Simos, o vyresni galės pajodinėti žirgais. Jeigu orai palankūs – maudynės ir vandens mūšiai garantuoti! Taip pat susipažinsime su avytėmis, ožiukais, maitinsime žąsiukus ir tyrinėsime gamtą Nemuno kilpų regioniniame parke.🕘 Atvykimas – 9:00 🕔 Išvykimas – 17:00 🍽 Kaina vaikui – 35 € (įskaičiuoti pietūs ir užkandžiai) 👫 Grupėje – 10–14 vaikų, tad dėmesio užteks kiekvienam!
 
 📍Vieta: Nemuno g. 10, Nibriai, Jiezno sen., Prienų r.
 📞 Registracija: 0606 43952,
@@ -30,9 +23,9 @@ Jeigu orai palankūs – maudynės ir vandens mūšiai garantuoti! Taip pat susi
       title: "Nauja svetainė!",
       date: "2025-07-29",
       image: "/lovable-uploads/77edc4ed-efa7-49d5-b2aa-c62ab59bf99e.png",
-      excerpt: "Džiaugiamės pristačiusi naują komandos narę - profesionalią fotografę Justę, kuri specializuojasi gyvūnų fotografijoje.",
+      excerpt: "",
       content: "Sveikiname Justę, kuri prisijungia prie mūsų komandos kaip profesionali fotografė! Justė turi 8 metų patirtį gyvūnų fotografijoje ir jau spėjo suorganizuoti kelias sėkmingas fotosesijas mūsų žirgynėlyje. Nuo šiol galėsime pasiūlyti dar aukštesnės kokybės fotosesijų paslaugas.",
-      readTime: "2 min skaitymo"
+      
     },
     {
       id: 3,
